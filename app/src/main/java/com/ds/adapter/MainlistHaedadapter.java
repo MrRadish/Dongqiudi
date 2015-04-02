468548050e07d377a723521fdd4f057a92c6ebe4
MrRadish <ds335687533@163.com>
2015-04-02 17:18:43 +0800

    Rejected commit: Default

--- app/src/main/java/com/ds/adapter/MainlistHaedadapter.java
+++ app/src/main/java/com/ds/adapter/MainlistHaedadapter.java
@@ -1,9 +1,23 @@
 package com.ds.adapter;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.widget.Toast;
 
+import com.ds.entity.ArticleHead;
+import com.ds.fragment.MainListHeadFragment;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,18 +26,25 @@ import java.util.List;
  */
 public class MainlistHaedadapter extends FragmentPagerAdapter {
     private List<Fragment> fragments;
-    public MainlistHaedadapter(FragmentManager fm,List<Fragment> fragments) {
+    private List<ArticleHead> data;
+    public MainlistHaedadapter(FragmentManager fm,List<Fragment> fragments,List<ArticleHead> data) {
         super(fm);
         this.fragments=fragments;
+        this.data=data;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return fragments.get(position);
+        MainListHeadFragment fragment= (MainListHeadFragment) fragments.get(position);
+        Bundle bundle=new Bundle();
+        bundle.putSerializable("entity",data.get(position));
+        fragment.setArguments(bundle);
+        return fragment;
     }
 
     @Override
     public int getCount() {
         return fragments.size();
     }
+
 }
